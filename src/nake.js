086d97f2efb89c51040b4886053f3e82106d569b
Benjamin Winterberg <winterbe@googlemail.com>
2014-04-08 15:25:40 +0200

    introduce local scope for nake internal stuff

--- src/nake.js
+++ src/nake.js
@@ -1,48 +1,68 @@
 #!/usr/bin/jjs -scripting
 
-var File = Java.type("java.io.File");
+var global = this;
 
-$EXEC("pwd");
+(function() {
+  var File = Java.type("java.io.File");
 
-var path = $OUT.trim() + "/Nakefile";
-
-var nakefile = new File(path);
-
-if (!nakefile.exists()) {
-  print('Nakefile does not exist');
-  exit();
-}
-
-var tasks = {};
-
-var task = function(name, description, action) {
-  tasks[name] = {
-    name: name,
-    description: description,
-    action: action
+  var fatalError = function (message) {
+    print(message);
+    exit(1);
   };
-};
 
-var printTasks = function() {
-  print('Tasks:');
-  for (var task in tasks) {
-    print(" - ${task}");
+
+  $EXEC("pwd");
+
+  global.path = $OUT.trim();
+
+  var nakefile = new File(global.path + "/Nakefile");
+
+  if (!nakefile.exists()) {
+    fatalError("Nakefile not found: ${global.path}");
   }
-};
 
-// evaluate Nakefile
-load(nakefile.getAbsolutePath());
+  var tasks = {};
 
-if (arguments.length == 0) {
-  printTasks();
-  exit(0);
-}
+  var task = function(name, description, action) {
+    tasks[name] = {
+      name: name,
+      description: description,
+      action: action
+    };
+  };
 
-var taskName = arguments[0];
-var context = tasks[taskName];
-if (!context) {
-  print("unknown task: ${taskName}");
-  exit(1);
-}
+  global.task = task;
 
-context.action.call(this);
+  var printTasks = function() {
+    print("Tasks defined in ${global.path}/Nakefile:");
+    for (var taskName in tasks) {
+      var task = tasks[taskName];
+      print(" - ${task.name} [${task.description}]");
+    }
+    print("\nuse 'nake -- taskName' to execute a task");
+  };
+
+
+  // evaluate Nakefile
+  load(nakefile.getAbsolutePath());
+
+
+  if (global.arguments.length == 0) {
+    printTasks();
+    exit(0);
+  }
+
+  var taskName = global.arguments[0];
+  var currentTask = tasks[taskName];
+  if (!currentTask) {
+    fatalError("no such task: ${taskName}\nuse 'nake' to list all available tasks");
+  }
+
+  try {
+    currentTask.action.call(global);
+  }
+  catch (e) {
+    fatalError("failed to execute task ${currentTask.name}: ${e}");
+  }
+
+})();
