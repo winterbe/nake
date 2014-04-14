dcf59b6e15aea801eba3e20aa4c0f3d85024636d
Benjamin Winterberg <winterbe@googlemail.com>
2014-04-14 07:25:44 +0200

    add comments

--- src/nake.js
+++ src/nake.js
@@ -1,7 +1,11 @@
 #!/usr/bin/jjs -scripting
 
+
+// tasks will be run in this context
 var global = this;
 
+
+// local scope for nake internal stuff
 (function() {
   var File = Java.type("java.io.File");
 
@@ -10,8 +14,10 @@ var global = this;
     exit(1);
   };
 
+
   global.path = $ENV['PWD'];
 
+  // find nearest Nakefile for current directory
   var findClosestNakefile = function (path) {
     var nakefile = new File(path + "/Nakefile");
     if (nakefile.exists()) {
@@ -30,8 +36,10 @@ var global = this;
     fatalError("no Nakefile found for directory: ${global.path}");
   }
 
+
   var tasks = {};
 
+  // register a task by name
   var task = function(name, description, action) {
     tasks[name] = {
       name: name,
@@ -43,13 +51,14 @@ var global = this;
   global.task = task;
 
 
-  var run = function (name, options) {
+  // run a specific task by name
+  var run = function (name, taskArgs) {
     var currentTask = tasks[name];
     if (!currentTask) {
       fatalError("no such task: ${taskName}\nuse 'nake' to list all available tasks");
     }
     try {
-      currentTask.action.call(global, options);
+      currentTask.action.call(global, taskArgs);
     }
     catch (e) {
       fatalError("execution of task ${currentTask.name} failed: ${e}");
@@ -57,6 +66,7 @@ var global = this;
   };
 
   global.run = run;
+
 
   var printTasks = function() {
     print("Tasks defined in ${global.path}/Nakefile\n");
@@ -86,23 +96,24 @@ var global = this;
 
   var args = $ARG;
 
-  if (args.length == 0) {
+  if (args.length === 0) {
     printTasks();
     exit(0);
   }
 
   var taskName = args[0];
-  var options = [];
+  var taskArgs = [];
   if (args.length > 1) {
-    options = args.slice(1);
+    taskArgs = args.slice(1);
   }
   var currentTask = tasks[taskName];
   if (!currentTask) {
     fatalError("no such task: ${taskName}\nuse 'nake' to list all available tasks");
   }
 
+  // call task with global context
   try {
-    currentTask.action.call(global, options);
+    currentTask.action.call(global, taskArgs);
   }
   catch (e) {
     fatalError("execution of task ${currentTask.name} failed: ${e}");
